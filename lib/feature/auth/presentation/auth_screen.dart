03cb221129e755312cdbe85b59e7eb7b288347f2
Kaparovvv <kaparovkanagat@gmail.com>
2023-02-09 00:21:09 +0600

    autorouteguard

--- lib/feature/auth/presentation/auth_screen.dart
+++ lib/feature/auth/presentation/auth_screen.dart
@@ -1,20 +1,30 @@
+import 'dart:developer';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:megalab_news_app/commons/images_helper.dart';
+import 'package:megalab_news_app/commons/textStyle_helper.dart';
 import 'package:megalab_news_app/commons/theme_helper.dart';
 import 'package:megalab_news_app/commons/validates_helper.dart';
 import 'package:megalab_news_app/core/global_widgets/custom_button_widget.dart';
 import 'package:megalab_news_app/core/global_widgets/custom_snackbar.dart';
+import 'package:megalab_news_app/core/global_widgets/custom_textbutton_widget.dart';
 import 'package:megalab_news_app/core/global_widgets/loading_indicator_widget.dart';
 import 'package:megalab_news_app/core/global_widgets/textfield_with_text_widget.dart';
 import 'package:megalab_news_app/core/router/app_router.gr.dart';
 import 'package:megalab_news_app/feature/auth/presentation/bloc/auth_bloc.dart';
 import 'package:megalab_news_app/feature/register/presentation/local_widgets/password_textfield_widget.dart';
+import 'package:megalab_news_app/utils/dependencies_injection.dart';
+
+import '../../../commons/names_helper.dart';
+import '../../../utils/dependencies_export.dart';
 
 class AuthScreen extends StatefulWidget {
-  const AuthScreen({super.key});
+  final bool isBackButton;
+  final void Function(bool isLoggedIn)? onLoginResult;
+  const AuthScreen({super.key, this.isBackButton = false, this.onLoginResult});
 
   @override
   State<AuthScreen> createState() => _AuthScreenState();
@@ -42,13 +52,15 @@ class _AuthScreenState extends State<AuthScreen> {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        leading: IconButton(
-          onPressed: () => context.router.navigateBack(),
-          icon: const Icon(
-            Icons.arrow_back_ios,
-            color: ThemeHelper.color7E5BC2,
-          ),
-        ),
+        leading: widget.isBackButton
+            ? IconButton(
+                onPressed: () => context.router.navigateBack(),
+                icon: const Icon(
+                  Icons.arrow_back_ios,
+                  color: ThemeHelper.color7E5BC2,
+                ),
+              )
+            : const SizedBox(),
       ),
       body: SafeArea(
         child: Center(
@@ -87,6 +99,7 @@ class _AuthScreenState extends State<AuthScreen> {
                           context.router.replace(const NewsListScreenRoute());
                           nicknameController.dispose();
                           passwordController.dispose();
+                          widget.onLoginResult?.call(true);
                         }
 
                         if (state is ErrorAuthState) {
@@ -100,6 +113,7 @@ class _AuthScreenState extends State<AuthScreen> {
                         return CustomButtonWidget(
                           txtButton: 'Войти',
                           onPressed: () {
+                            log('User Token =========== ${getIt.get<SharedPreferences>().getString(NamesHelper.cacheUserToken)}');
                             if (_formkey.currentState!.validate()) {
                               _authBloc.add(
                                 AuthUserEvent(
@@ -112,6 +126,27 @@ class _AuthScreenState extends State<AuthScreen> {
                           width: 168,
                         );
                       },
+                    ),
+                    SizedBox(height: 16.h),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'У вас нет аккаунта?',
+                          style: TextStyleHelper.f12w400
+                              .copyWith(color: ThemeHelper.color5A5A5A),
+                        ),
+                        CustomTextButtonWidget(
+                          onPressed: () => context.router.push(
+                            const RegistrationScreenRoute(),
+                          ),
+                          text: 'зарегистрироваться',
+                          textStyle: TextStyleHelper.f12w400.copyWith(
+                            color: ThemeHelper.color2D4EC2,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
